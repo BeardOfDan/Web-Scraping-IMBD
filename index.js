9dc2fdf8d2f7fb24e881d2518f13f9729c728e61
Daniel Roberts <BeardOfDan@gmail.com>
2018-11-23 06:22:52 -0800

    (feat) Get the data

--- index.js
+++ index.js
@@ -7,8 +7,40 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.get('/scrape', (req, res, next) => {
-  // scraping will happen here
-});
+  const url = 'http://www.imdb.com/title/tt1229340/';
+
+  request(url, (err, res, html) => {
+    if (!err) {
+      const $ = cheerio.load(html);
+
+      let title;
+      let release;
+      let rating;
+
+      const json = { title: '', release: '', rating: '' };
+
+      $('header').filter(() => {
+        const data = $(this);
+
+        title = data.children().first().text();
+
+        json.title = title;
+
+        release = data.children().last().children().text();
+
+        json.release = release;
+      });
+
+      $('.star-box-giga-star').filter(() => {
+        const data = $(this);
+
+        rating = data.text();
+        json.rating = rating;
+      });
+
+    }
+  });
+}); // end of get '/scrape'
 
 app.listen(PORT, () => {
   console.log(`Listening on port: ${PORT}`);
