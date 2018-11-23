3ab84c87d75e5d678653256ea89a2e4e08ab6960
Daniel Roberts <BeardOfDan@gmail.com>
2018-11-23 07:27:44 -0800

    (fix) Correctly find rating in DOM

--- index.js
+++ index.js
@@ -9,40 +9,29 @@ const PORT = process.env.PORT || 5000;
 app.get('/scrape', (req, res, next) => {
   const url = 'http://www.imdb.com/title/tt1229340/';
 
-  const json = { title: '', release: '', rating: ''};
+  const json = { title: '', release: '', rating: '' };
 
   request(url, (err, response, html) => {
     if (err) {
       console.log('ERROR: ' + err);
-      return res.send(err);
-    } else { // no error
-      const $ = cheerio.load(html);
 
-      let rating;
+      return res.send(err);
+    } else {
+      const $ = cheerio.load(html);
 
       $('h1').filter(function () {
         const data = $(this);
-        let title;
-
-        title = data.contents().first().text();
-        title = title.slice(0, title.length - 1); // remove trailing space
-
-        json.title = title;
+        json.title = data.contents().first().text().trim();
       });
 
       $('#titleYear').filter(function () {
         const data = $(this);
-        json.release = data.children().first().text();
+        json.release = data.children().first().text().trim();
       });
 
-      $('.star-box-giga-star').filter(function () {
+      $(`.ratingValue`).filter(function () {
         const data = $(this);
-
-        rating = data.text();
-
-        console.log('rating: ' + rating);
-
-        json.rating = rating;
+        json.rating = data.text().trim();
       });
     }
 
@@ -57,11 +46,7 @@ app.get('/scrape', (req, res, next) => {
     // a message to a browser that tries to use this path
     return res.send('Check your console');
   }); // end of request
-
-
 }); // end of get '/scrape'
-
-
 
 app.listen(PORT, () => {
   console.log(`Listening on port: ${PORT}`);
