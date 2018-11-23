e0702414766f67f26f8fc92fc65145d128482c37
Daniel Roberts <BeardOfDan@gmail.com>
2018-11-23 06:46:32 -0800

    (fixes) Multiple
    One asynchronous issue and one function variety issue.

--- index.js
+++ index.js
@@ -9,48 +9,61 @@ const PORT = process.env.PORT || 5000;
 app.get('/scrape', (req, res, next) => {
   const url = 'http://www.imdb.com/title/tt1229340/';
 
-  const json = { title: '', release: '', rating: '' };
+  const json = { title: '', release: '', rating: '', time: Date.now() };
 
-  request(url, (err, res, html) => {
-    if (!err) {
+  request(url, (err, response, html) => {
+    if (err) {
+      console.log('ERROR: ' + err);
+      return res.send(err);
+    } else { // no error
+      console.log('no err...');
+
       const $ = cheerio.load(html);
 
       let title;
       let release;
       let rating;
 
-      $('header').filter(() => {
+
+      $('.header').filter(function () {
         const data = $(this);
 
         title = data.children().first().text();
+
+        console.log('title: ' + title);
 
         json.title = title;
 
         release = data.children().last().children().text();
 
+        console.log('release: ' + release);
+
         json.release = release;
       });
 
-      $('.star-box-giga-star').filter(() => {
+      $('.star-box-giga-star').filter(function () {
         const data = $(this);
 
         rating = data.text();
+
+        console.log('rating: ' + rating);
+
         json.rating = rating;
       });
-
     }
+
+    fs.writeFile('output.json', JSON.stringify(json, '', 2), (err) => {
+      if (err) {
+        console.log(`\nERROR!:\n${err}`);
+      } else {
+        console.log('File successfully written to \'output.json\'');
+      }
+    });
+
+    // a message to a browser that tries to use this path
+    return res.send('Check your console');
   }); // end of request
 
-  fs.writeFile('output.json', JSON.stringify(json, '', 2), (err) => {
-    if (err) {
-      console.log(`\nERROR!:\n${err}`);
-    } else {
-      console.log('File successfully written to \'output.json\'');
-    }
-  });
-
-  // a message to a browser that tries to use this path
-  return res.send('Check your console');
 
 }); // end of get '/scrape'
 
